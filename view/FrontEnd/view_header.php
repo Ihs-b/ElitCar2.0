9b8b25e0025584b605a034f181a95d92650d6f89
kaouther09 <mouzaouirihab@gmail.com>
2024-07-01 14:25:30 +0200

    gome page commit

---
+++ view/FrontEnd/view_header.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+       <nav class="navbar">
+         <div class="container">
+
+                <div class="navbar-header">
+                        <button class="navbar-toggler" data-toggle="open-navbar1"></button>
+                        <a href="homepage.php"><h4>Elit<span>Car</span></h4></a>   
+                </div>
+
+                    <div class="navbar-menu" id="open-navbar1">
+                    <ul class="navbar-nav">
+                        <!-- <li class="active"><a href="#">Se connecter</a></li> -->
+                        <li class="navbar-dropdown">
+                        <a href="#" class="dropdown-toggler" data-dropdown="my-dropdown-id">Se connecter <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown" id="my-dropdown-id">
+                            <li><a href="#">Admin</a></li>
+                            <li class="separator"></li>
+                            <li><a href="#">Professionnel</a></li>
+                            <li class="separator"></li>
+                            <li><a href="#">particulier</a></li>
+                        </ul>
+                        </li>
+                        <li><a href="#">Agences</a></li>
+                        <li><a href="#">F&Q</a></li>
+                       
+                    </ul>
+                    </div>
+                </div>
+                </nav>
+</header>
