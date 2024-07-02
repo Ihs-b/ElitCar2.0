9c90fefd17ef215a6f04c572faada43eedf90e9c
sane ben <tefl.ben10@gmail.com>
2024-07-02 22:36:53 +0200

    title added and css file renamed

--- view/FrontEnd/view_profile_particulier.php
+++ view/FrontEnd/view_profile_particulier.php
@@ -1,11 +1,21 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elitcar</title>
-    <link href="http://localhost/ElitCar2.0/public/css/styles.css" rel="stylesheet">
-</head>
+<?php
+$title = "profileCust";
+$link ='
+    <link rel="stylesheet" href="././public/css/particulier_styles.css">
+    ';
+$script ='<script src="././public/js/script.js">
+</script>';
+ob_start();
+?>
+
+<!--<!DOCTYPE html>-->
+<!--<html lang="fr">-->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+<!--    <title>Elitcar</title>-->
+<!--    <link href="http://localhost/ElitCar2.0/public/css/particulier_styles.css" rel="stylesheet">-->
+<!--</head>-->
 <body>
 <!--<header>-->
 <!--    <div class="header-left">-->
@@ -178,3 +188,7 @@
 <script src="http://localhost/ElitCar2.0/public/js/script.js"></script>
 </body>
 </html>
+<?php
+$content = ob_get_clean();
+require('view_template.php');
+?>
